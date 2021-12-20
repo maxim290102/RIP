f88fdffdbe159f730d6bc415281bd9ddfaf5ca84
Сергей <maxim.2002.k@mail.ru>
2021-12-20 22:08:28 +0300

    rk2

--- lab4/lab4/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from lab4 import views
-
-urlpatterns = [
-    path('', views.menu),
-    path('page/<int:id>/', views.page, name='page_url'),
-]
--- lab4/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.master, name="index"),
-    path('<int:mem_id>/', views.detail, name="about")
-]
